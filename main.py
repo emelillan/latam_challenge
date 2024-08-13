0fcf5ed9e6c085f297dd799969359faf784095fd
lastname <ezequiel.melillan@cardinalpath.com>
2024-08-13 19:42:11 -0300

    trigger CICD pipe - changes in CF - Adding GCS functionallity

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ import functions_framework
 @functions_framework.http
 def main(request):
     print("working")
+    # Get data from GCS using Cloud Storage client library
+    # storage_client = storage.Client()
+    # bucket = storage_client.bucket('my-bucket')
+    # blob = bucket.blob('my-file')
+    # data = blob.download_as_string()
+    # print(data)
     return 'Hello, World!'
