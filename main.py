6482a5c49569c1b0711378d33ace0e09a4fab5ec
lastname <ezequiel.melillan@cardinalpath.com>
2024-08-13 19:49:12 -0300

    trigger CICD pipe - changes in CF - Adding GCS functionallity - uploading file to GCS

--- main.py
+++ main.py
@@ -3,15 +3,20 @@
 
 # Import the 'functionframework' package
 import functions_framework
+from google.cloud import storage
+import pandas as pd
+import json
 
 
 @functions_framework.http
 def main(request):
     print("working")
     # Get data from GCS using Cloud Storage client library
-    # storage_client = storage.Client()
-    # bucket = storage_client.bucket('my-bucket')
-    # blob = bucket.blob('my-file')
-    # data = blob.download_as_string()
-    # print(data)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('latam')
+    blob = bucket.blob('farmers-protest-tweets-2021-2-4.json')
+    data = blob.download_as_string()
+    json_data = json.loads(data)
+    df = pd.DataFrame(json_data)
+    print(df.head())
     return 'Hello, World!'
