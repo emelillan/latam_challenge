ebb77e0e51b92b26dd1bc30a5e4fabeb188c5aba
lastname <ezequiel.melillan@cardinalpath.com>
2024-08-14 16:44:04 -0300

    generator added for all the funcs in main.py

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-# dummy http GCP cloud function using functionframework
+# Description: This file contains the main function that will be executed when the cloud function is triggered.
 
-
-# Import the 'functionframework' package
 import functions_framework
 from google.cloud import storage
-import pandas as pd
 import json
+
 from src.q1_memory import q1_memory
+from src.q2_memory import q2_memory
+from src.q3_memory import q3_memory
+
+from src.q1_time import q1_time
+from src.q2_time import q2_time
+from src.q3_time import q3_time
+
+
+generator_functions = {
+    "q1_memory": q1_memory,
+    "q2_memory": q2_memory,
+    "q3_memory": q3_memory,
+    "q1_time": q1_time,
+    "q2_time": q2_time,
+    "q3_time": q3_time,
+}
 
 
 @functions_framework.http
 def main(request):
 
     # Get request data
-    request_json = request.get_json()
-    print(request_json)
+    payload = request.get_json()
+    function = payload['function'].lower()
+    function_to_call = generator_functions[function]
+
     # Get data from GCS using Cloud Storage client library
     storage_client = storage.Client()
     bucket = storage_client.bucket('latam-challenge')
     blob = bucket.blob('farmers-protest-tweets-2021-2-4.json')
     data = blob.download_as_string()
+
     # save data in the tmp folder
     with open('/tmp/data.json', 'wb') as f:
         f.write(data)
-    result = q1_memory('/tmp/data.json')
+
+    # call the function
+    result = function_to_call('/tmp/data.json')
     result = {str(k): v for k, v in result}
+
     # return the result as a json
     return json.dumps(result)
